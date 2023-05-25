4f7c3ad451cc717ad545f6c04b935a14864e6780
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-05-25 14:29:36 +0300

    betty

--- push.c
+++ push.c
@@ -18,24 +18,24 @@ void s_push(my_stack_t **head, unsigned int counter)
 		{
 			if (bus.arg[j] > 57 || bus.arg[j] < 48)
 				flag = 1;
-    }
+		}
 		if (flag == 1)
 		{
-      fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
 			free_stack(*head);
 			exit(EXIT_FAILURE);
-    }
-  }
+		}
+	}
 	else
 	{
-    fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
-  }
+	}
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
 		add_node(head, n);
