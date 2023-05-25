62ced2ea9180eb4832bc36de52a2ad92e3315449
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-05-25 11:05:45 +0300

    add c files

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	return (0);
+}
